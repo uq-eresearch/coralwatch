eb6e20c22f6a36d2cc2a92cb83279942eb0b9c5e
Peter Becker <pbecker@itee.uq.edu.au>
2009-07-23 01:19:57 +0000

    Changed code to use MD5 hash generation from utility module.
    The original code has been moved into the utility module for reuse.

--- coralwatch-webapp/src/main/java/org/coralwatch/resources/UserResource.java
+++ coralwatch-webapp/src/main/java/org/coralwatch/resources/UserResource.java
@@ -6,25 +6,20 @@ import au.edu.uq.itee.maenad.restlet.errorhandling.NoDataFoundException;
 import au.edu.uq.itee.maenad.restlet.errorhandling.SubmissionError;
 import au.edu.uq.itee.maenad.restlet.errorhandling.SubmissionException;
 import au.edu.uq.itee.maenad.util.BCrypt;
+import au.edu.uq.itee.maenad.util.HashGenerator;
+
 import org.coralwatch.app.CoralwatchApplication;
 import org.coralwatch.dataaccess.UserDao;
 import org.coralwatch.model.UserImpl;
 import org.restlet.data.Form;
 import org.restlet.resource.Variant;
 
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
 public class UserResource extends ModifiableEntityResource<UserImpl, UserDao, UserImpl> {
-    private static final Logger LOGGER = Logger.getLogger(UserResource.class.getName());
-
     public UserResource() throws InitializationException {
         super(CoralwatchApplication.getConfiguration().getUserDao());
     }
@@ -34,39 +29,9 @@ public class UserResource extends ModifiableEntityResource<UserImpl, UserDao, Us
         super.fillDatamodel(datamodel);
         UserImpl userImpl = (UserImpl) datamodel.get(getTemplateObjectName());
         datamodel.put("conductedSurveys", getDao().getSurveyEntriesCreated(userImpl));
-        String emailHash = createMD5Hash(userImpl.getEmail());
+        String emailHash = HashGenerator.createMD5Hash(userImpl.getEmail());
         datamodel.put("gravatarUrl", "http://www.gravatar.com/avatar/" + emailHash + "?s=80&d=wavatar");
     }
-
-    private static String createMD5Hash(String input) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            byte[] hash = md.digest(input.getBytes("UTF-8"));
-            return bytesToString(hash);
-        } catch (NoSuchAlgorithmException e) {
-            // should really not happen since MD5 should be there
-            LOGGER.log(Level.SEVERE, "Failed to find MD5 codec", e);
-        } catch (UnsupportedEncodingException e) {
-            // should really not happen since UTF-8 is guaranteed
-            LOGGER.log(Level.SEVERE, "Failed to find UTF-8 encoding", e);
-        }
-        assert false : "We failed to MD5 encode a string.";
-        return null;
-    }
-
-    private static String bytesToString(byte[] bytes) {
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < bytes.length; i++) {
-            byte b = bytes[i];
-            String hex = Integer.toHexString(0x00FF & b);
-            if (hex.length() == 1) {
-                sb.append("0");
-            }
-            sb.append(hex);
-        }
-        return sb.toString();
-    }
-
 
     @Override
     protected void updateObject(UserImpl userImpl, Form form) throws SubmissionException {
