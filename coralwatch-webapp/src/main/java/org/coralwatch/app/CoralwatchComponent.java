c9b6c23d68f1c5e830c0c2db9600f9ac70637324
Peter Becker <pbecker@itee.uq.edu.au>
2009-07-28 07:19:03 +0000

    Bugfix: added missing protocol handlers for WAR version.
    By changing to the application-based configuration in the web.xml file we lost not only the server handler (which we wanted to get rid off), but also the client handlers, which meant we couldn't read files from the file system or classpath anymore. This should be fixed now.

--- coralwatch-webapp/src/main/java/org/coralwatch/app/CoralwatchComponent.java
+++ coralwatch-webapp/src/main/java/org/coralwatch/app/CoralwatchComponent.java
@@ -8,6 +8,7 @@ public class CoralwatchComponent extends Component {
 
     public CoralwatchComponent() {
         getServers().add(Protocol.HTTP, CoralwatchApplication.getConfiguration().getHttpPort());
+        // make sure to add protocols in web.xml, too
         getClients().add(Protocol.CLAP);
         getClients().add(Protocol.FILE);
         getDefaultHost().attach(new CoralwatchApplication());
