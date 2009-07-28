5026bf73e69e4925fb8afec7fdab00d857746bd6
Peter Becker <pbecker@itee.uq.edu.au>
2009-07-28 05:01:57 +0000

    Added basic login/logout tests.
    The main functionality for log in and out is present in the base class now, which allows for later reuse.

---
+++ coralwatch-test/src/test/java/org/coralwatch/BaselineTest.java
@@ -0,0 +1,26 @@
+package org.coralwatch;
+
+
+public class BaselineTest extends CoralWatchTestCase {
+    public void testFrontPageShows() {
+        assertTitleEquals("CoralWatch");
+        // check if end of page is rendered
+        assertTextPresent("If you have any queries or comments please email");
+        assertTextPresent("CoralWatch Supporters");
+        // check menu is rendered
+        assertLinkPresentWithText("Login");
+    }
+
+    public void testLoginLogout() {
+        loginAsAdmin();
+        assertTextPresent("Abdul Alabri");
+        assertLinkPresentWithExactText("New Survey");
+        logout();
+        assertTextPresent("You successfully logged out.");
+        loginAsUser();
+        assertTextPresent("Dave Logan");
+        assertLinkPresentWithExactText("New Survey");
+        logout();
+        assertTextPresent("You successfully logged out.");
+    }
+}
--- coralwatch-test/src/test/java/org/coralwatch/CoralWatchTestCase.java
+++ coralwatch-test/src/test/java/org/coralwatch/CoralWatchTestCase.java
@@ -2,10 +2,34 @@ package org.coralwatch;
 
 import net.sourceforge.jwebunit.junit.WebTestCase;
 
+/**
+ * A base class that handles aspects common to all tests against CoralWatch.
+ */
 public abstract class CoralWatchTestCase extends WebTestCase {
     public void setUp() throws Exception {
         super.setUp();
         String baseUrl = System.getProperty("coralwatch.baseUrl", "http://localhost:9635");
         setBaseUrl(baseUrl);
+        beginAt("/");
+        // TODO set up test fixtures
+    }
+
+    protected void login(String username, String password) {
+        clickLinkWithExactText("Login");
+        setTextField("username", username);
+        setTextField("password", password);
+        clickButton("loginButton");
+    }
+
+    protected void loginAsAdmin() {
+        login("admin", "admin");
+    }
+
+    protected void loginAsUser() {
+        login("user", "user");
+    }
+
+    protected void logout() {
+        clickLinkWithExactText("Logout");
     }
 }
