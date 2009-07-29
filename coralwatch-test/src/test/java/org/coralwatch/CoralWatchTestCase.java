c35a6c47ba0a031709b61000177774f36ef98dbd
Peter Becker <pbecker@itee.uq.edu.au>
2009-07-29 05:44:48 +0000

    Added a bit of a sleep after clicks to avoid race conditions with Dojo.
    It seemed that sometimes we try to click things before the JavaScript code on the page was fully executed. I'm not entirely sure if that is correct and I don't know if sleeping 200ms is going to fix it consistently, but I don't have the time to solve the problem properly. This might fix the problem at least on the build server -- we'll see.

--- coralwatch-test/src/test/java/org/coralwatch/CoralWatchTestCase.java
+++ coralwatch-test/src/test/java/org/coralwatch/CoralWatchTestCase.java
@@ -32,4 +32,33 @@ public abstract class CoralWatchTestCase extends WebTestCase {
     protected void logout() {
         clickLinkWithExactText("Logout");
     }
+
+    public void clickButton(String buttonId) {
+        super.clickButton(buttonId);
+        waitForDojo();
+    }
+
+    public void clickLink(String linkId) {
+        super.clickLink(linkId);
+        waitForDojo();
+    }
+
+    public void clickLinkWithExactText(String linkText) {
+        super.clickLinkWithExactText(linkText);
+        waitForDojo();
+    }
+
+    /**
+     * Waits for some milliseconds to let Dojo do its thing.
+     * 
+     * It seems that sometimes we get race conditions between our calls and Dojo's changing
+     * of widgets. We should find a way to synchronize properly, but for now we just wait.
+     */
+    protected void waitForDojo() {
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            // should not happen
+        }
+    }
 }
