6c92dddd9a73d27caa17a62446b61cd8a56a7a01
Peter Becker <pbecker@itee.uq.edu.au>
2009-07-29 01:32:53 +0000

    Added serialVersionUID.
    Not that we intend to use anything needing it, but it makes Eclipse happy.

--- coralwatch-webapp/src/main/java/org/coralwatch/model/KitRequest.java
+++ coralwatch-webapp/src/main/java/org/coralwatch/model/KitRequest.java
@@ -14,6 +14,7 @@ import java.util.Date;
 
 @Entity
 public class KitRequest implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
