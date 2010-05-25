5a37b8c1f748b24604a946951b10fdb0653f3860
Abdulmonem Alabri <a.alabri@uq.edu.au>
2010-05-25 04:56:24 +0000

    Removed Reslet framwork related code

--- coralwatch-webapp/src/main/java/org/coralwatch/app/CoralwatchApplication.java
+++ coralwatch-webapp/src/main/java/org/coralwatch/app/CoralwatchApplication.java
@@ -1,20 +1,7 @@
 package org.coralwatch.app;
 
-import au.edu.uq.itee.maenad.restlet.AccessDeniedResource;
-import au.edu.uq.itee.maenad.restlet.CachingDirectory;
-import au.edu.uq.itee.maenad.restlet.StyleResource;
-import au.edu.uq.itee.maenad.restlet.SubmissionErrorResource;
-import au.edu.uq.itee.maenad.restlet.auth.AccessPolicy;
 import au.edu.uq.itee.maenad.restlet.errorhandling.InitializationException;
-import au.edu.uq.itee.maenad.restlet.model.FrameDataProvider;
-import org.coralwatch.resources.*;
-import org.coralwatch.resources.testing.DataExchangeResource;
-import org.coralwatch.resources.testing.ResetResource;
 import org.restlet.Application;
-import org.restlet.Directory;
-import org.restlet.Restlet;
-import org.restlet.Router;
-import org.restlet.data.LocalReference;
 
 public class CoralwatchApplication extends Application {
 
@@ -30,56 +17,5 @@ public class CoralwatchApplication extends Application {
 
     public static Configuration getConfiguration() {
         return configuration;
-    }
-
-    @Override
-    public Restlet createRoot() {
-
-        Router router = new Router(getContext());
-
-        getContext().getAttributes().put(FrameDataProvider.class.getName(), new org.coralwatch.resources.FrameDataProvider());
-        getContext().getAttributes().put(AccessPolicy.class.getName(), new CoralwatchAccessPolicy());
-
-        String baseUrl = configuration.getBaseUrl();
-        if (baseUrl != null) {
-            getContext().getAttributes().put("baseUrl", baseUrl);
-        }
-        router.attachDefault(FrontpageResource.class);
-        router.attach("/dashboard", DashboardResource.class);
-        router.attach("/surveys", SurveyListResource.class);
-        router.attach("/trustors", TrustSubTreeResource.class);
-        router.attach("/surveys/{id}", SurveyResource.class);
-        router.attach("/usertrust/{id}", UserTrustResource.class);
-        router.attach("/usertrust", UserTrustListResource.class);
-        router.attach("/surveyrating/{id}", SurveyRatingResource.class);
-        router.attach("/surveyrating", SurveyRatingListResource.class);
-        router.attach("/login", LoginResource.class);
-        router.attach("/oneoff", OneOffSubmissionResource.class);
-        router.attach("/logout", LogoutResource.class);
-        router.attach("/reef", ReefListResource.class);
-        router.attach("/reef/{id}", ReefResource.class);
-        router.attach("/submissionError", SubmissionErrorResource.class);
-        router.attach("/accessDenied", AccessDeniedResource.class);
-        router.attach("/style.css", StyleResource.class);
-        router.attach("/icons", new CachingDirectory(getContext(),
-                LocalReference.createClapReference(LocalReference.CLAP_THREAD, "/icons")));
-        router.attach("/javascript", new CachingDirectory(getContext(),
-                LocalReference.createClapReference(LocalReference.CLAP_THREAD, "/javascript")));
-        router.attach("/images", new CachingDirectory(getContext(),
-                LocalReference.createClapReference(LocalReference.CLAP_THREAD, "/images")));
-
-        if (getConfiguration().isTestSetup()) {
-            router.attach("/reset", ResetResource.class);
-            router.attach("/data", DataExchangeResource.class);
-        } else {
-            router.attach("/data", DataDownloadResource.class);
-        }
-
-        Directory downloadDirectory = new Directory(getContext(), "war:///files");
-        router.attach("/download", downloadDirectory);
-
-        setConnectorService(getConfiguration().getConnectorService());
-
-        return router;
     }
 }
