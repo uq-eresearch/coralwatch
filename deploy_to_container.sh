802cb4d384a0301dcf45fa4c12a1f5871e199252
Tim Dettrick <t.dettrick@uq.edu.au>
2017-10-16 09:50:06 +1000

    Compress Docker image

--- deploy_to_container.sh
+++ deploy_to_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./backup2swift backup -c container-upload.json target/coralwatch.war coralwatch.docker
+./backup2swift backup -c container-upload.json target/coralwatch.war coralwatch.docker.xz
