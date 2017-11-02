e9c9a1722c9dd5c7695e10821bd5498ed3ce7f21
Andre Gebers <a.gebers@uq.edu.au>
2017-11-03 09:49:11 +1000

    reverted to oracle jdk1.6.0_29 to fix ClassNotFoundException on image upload
    java.lang.ClassNotFoundException: com.sun.image.codec.jpeg.ImageFormatException
    	org.apache.catalina.loader.WebappClassLoader.loadClass(WebappClassLoader.java:1387)
    	org.apache.catalina.loader.WebappClassLoader.loadClass(WebappClassLoader.java:1233)
    	com.sun.media.jai.codecimpl.JPEGImageDecoder.decodeAsRenderedImage(JPEGImageDecoder.java:46)
    	com.sun.media.jai.codec.ImageDecoderImpl.decodeAsRenderedImage(ImageDecoderImpl.java:140)
    	com.liferay.portal.image.ImageProcessorImpl.read(ImageProcessorImpl.java:178)
    
    added workaround for sometimes disappearing portlets.

--- coralwatch-webapp/build_docker.sh
+++ coralwatch-webapp/build_docker.sh
@@ -10,6 +10,8 @@ if [ ! -f $CORALWATCH_DIR/target/coralwatch.war ]; then
 fi
 wget -P $DOCKER_DIR https://swift.rc.nectar.org.au:8888/v1/AUTH_96387d3104434db5bdd0a74e17e503f5/docker/liferay.tar.gz
 wget -P $DOCKER_DIR https://swift.rc.nectar.org.au:8888/v1/AUTH_96387d3104434db5bdd0a74e17e503f5/docker/setenv.sh
+wget -P $DOCKER_DIR https://swift.rc.nectar.org.au:8888/v1/AUTH_96387d3104434db5bdd0a74e17e503f5/docker/jdk1.6.0_29.tar.gz
+wget -P $DOCKER_DIR https://swift.rc.nectar.org.au:8888/v1/AUTH_96387d3104434db5bdd0a74e17e503f5/docker/coralwatch.sh
 cp $CORALWATCH_DIR/Dockerfile $DOCKER_DIR
 cp $CORALWATCH_DIR/target/coralwatch.war $DOCKER_DIR
 ( cd $DOCKER_DIR && docker build -t coralwatch . )
