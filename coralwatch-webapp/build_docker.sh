4f9ac84a20bb97354c29d2839584af6ae4ff096e
Robert Dalla <robertdalla@users.noreply.github.com>
2018-05-24 00:54:33 +1000

    Update build_docker.sh

--- coralwatch-webapp/build_docker.sh
+++ coralwatch-webapp/build_docker.sh
@@ -18,7 +18,8 @@ wget -P $DOCKER_DIR https://swift.rc.nectar.org.au:8888/v1/AUTH_96387d3104434db5
 cp $CORALWATCH_DIR/Dockerfile $DOCKER_DIR
 cp $CORALWATCH_DIR/target/coralwatch.war $DOCKER_DIR
 
-cd ../$CORALWATCH_DIR
+cd $CORALWATCH_DIR
+cd ..
 ls -la
 
 ls -la $DOCKER_DIR
