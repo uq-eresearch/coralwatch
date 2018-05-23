7826e671258a01e093a6c8eea5be50296bbd056b
Robert Dalla <robertdalla@users.noreply.github.com>
2018-05-24 01:45:37 +1000

    Update build_docker.sh

--- coralwatch-webapp/build_docker.sh
+++ coralwatch-webapp/build_docker.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-CORALWATCH_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-DOCKER_DIR=${1:-$CORALWATCH_DIR/target/docker}
-CORALWATCH_THEME="$( dirname "$CORALWATCH_DIR" )"
+CORALWATCH_WEBAPP="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DOCKER_DIR=${1:-$CORALWATCH_WEBAPP/target/docker}
+CORALWATCH_DIR="$( dirname "$CORALWATCH_WEBAPP" )"
 
 rm -rf $DOCKER_DIR
 mkdir -p $DOCKER_DIR
-if [ ! -f $CORALWATCH_DIR/target/coralwatch.war ]; then
-  mvn -f $CORALWATCH_DIR/pom.xml clean package
+if [ ! -f $CORALWATCH_WEBAPP/target/coralwatch.war ]; then
+  mvn -f $CORALWATCH_WEBAPP/pom.xml clean package
 fi
 
 wget -P $DOCKER_DIR https://swift.rc.nectar.org.au:8888/v1/AUTH_96387d3104434db5bdd0a74e17e503f5/docker/liferay.tar.gz
@@ -16,8 +16,8 @@ wget -P $DOCKER_DIR https://swift.rc.nectar.org.au:8888/v1/AUTH_96387d3104434db5
 wget -P $DOCKER_DIR https://swift.rc.nectar.org.au:8888/v1/AUTH_96387d3104434db5bdd0a74e17e503f5/docker/jdk1.6.0_29.tar.gz
 wget -P $DOCKER_DIR https://swift.rc.nectar.org.au:8888/v1/AUTH_96387d3104434db5bdd0a74e17e503f5/docker/coralwatch.sh
 
-cp $CORALWATCH_DIR/Dockerfile $DOCKER_DIR
-cp $CORALWATCH_DIR/target/coralwatch.war $DOCKER_DIR
-cp -R $CORALWATCH_THEME $DOCKER_DIR/coralwatch-theme
+cp $CORALWATCH_WEBAPP/Dockerfile $DOCKER_DIR
+cp $CORALWATCH_WEBAPP/target/coralwatch.war $DOCKER_DIR
+cp -R CORALWATCH_DIR/coralwatch-theme $DOCKER_DIR/coralwatch-theme
 
 ls -la $DOCKER_DIR/coralwatch-theme
