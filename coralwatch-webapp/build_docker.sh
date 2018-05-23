0d3ac61839b58ee6e2238aa49ad1d07d3f542989
Robert Dalla <robertdalla@users.noreply.github.com>
2018-05-24 03:52:24 +1000

    Update build_docker.sh

--- coralwatch-webapp/build_docker.sh
+++ coralwatch-webapp/build_docker.sh
@@ -17,7 +17,7 @@ wget -P $DOCKER_DIR https://swift.rc.nectar.org.au:8888/v1/AUTH_96387d3104434db5
 wget -P $DOCKER_DIR https://swift.rc.nectar.org.au:8888/v1/AUTH_96387d3104434db5bdd0a74e17e503f5/docker/coralwatch.sh
 
 cp $CORALWATCH_WEBAPP/Dockerfile $DOCKER_DIR
-cp $CORALWATCH_WEBAPP/target/coralwatch.war $DOCKER_DIR
+echo cp $CORALWATCH_WEBAPP/target/coralwatch.war $DOCKER_DIR
 cp -R $CORALWATCH_DIR/coralwatch-theme/docroot/_diffs $DOCKER_DIR/coralwatch-theme
 
 ( cd $DOCKER_DIR && docker build -t coralwatch . )
