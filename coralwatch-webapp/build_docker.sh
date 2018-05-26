d7e6281f6b91759c85584f082710c2ad8b25ca03
Robert Dalla <robertdalla@users.noreply.github.com>
2018-05-26 21:21:06 +1000

    Update build_docker.sh

--- coralwatch-webapp/build_docker.sh
+++ coralwatch-webapp/build_docker.sh
@@ -11,13 +11,15 @@ if [ ! -f $CORALWATCH_WEBAPP/target/coralwatch.war ]; then
   echo mvn -f $CORALWATCH_WEBAPP/pom.xml clean package
 fi
 
-curl https://swift.rc.nectar.org.au:8888/v1/AUTH_96387d3104434db5bdd0a74e17e503f5/docker/liferay.tar.gz | tar xz --directory=$DOCKER_DIR
 wget -P $DOCKER_DIR https://swift.rc.nectar.org.au:8888/v1/AUTH_96387d3104434db5bdd0a74e17e503f5/docker/setenv.sh
 wget -P $DOCKER_DIR https://swift.rc.nectar.org.au:8888/v1/AUTH_96387d3104434db5bdd0a74e17e503f5/docker/jdk1.6.0_29.tar.gz
 wget -P $DOCKER_DIR https://swift.rc.nectar.org.au:8888/v1/AUTH_96387d3104434db5bdd0a74e17e503f5/docker/coralwatch.sh
 
 cp $CORALWATCH_WEBAPP/Dockerfile $DOCKER_DIR
 echo cp $CORALWATCH_WEBAPP/target/coralwatch.war $DOCKER_DIR
+
+curl https://swift.rc.nectar.org.au:8888/v1/AUTH_96387d3104434db5bdd0a74e17e503f5/docker/liferay.tar.gz | tar xz --directory=$DOCKER_DIR
 cp -R $CORALWATCH_DIR/coralwatch-theme/docroot/_diffs/* $DOCKER_DIR/liferay/tomcat-6.0.18/webapps/coralwatch-theme
+tar -zcvf $DOCKER_DIR/liferay.tar.gz $DOCKER_DIR/liferay
 
 ( cd $DOCKER_DIR && docker build -t coralwatch . )
