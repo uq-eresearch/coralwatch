9c0d9aecaef5d2719bfcb52b2406df6d57ff9b03
Robert Dalla <robertdalla@users.noreply.github.com>
2018-05-27 04:04:20 +1000

    Update build_docker.sh

--- coralwatch-webapp/build_docker.sh
+++ coralwatch-webapp/build_docker.sh
@@ -17,7 +17,9 @@ wget -P $DOCKER_DIR https://swift.rc.nectar.org.au:8888/v1/AUTH_96387d3104434db5
 
 wget -P $DOCKER_DIR https://swift.rc.nectar.org.au:8888/v1/AUTH_96387d3104434db5bdd0a74e17e503f5/docker/liferay.tar.gz
 sudo chown -R root:root $CORALWATCH_DIR/coralwatch-theme/docroot/_diffs
+sudo ls -la $CORALWATCH_DIR/coralwatch-theme/docroot/_diffs
 sudo tar -zcvf $DOCKER_DIR/coralwatch-theme_diffs.tar.gz $CORALWATCH_DIR/coralwatch-theme/docroot/_diffs
+sudo ls -ka $DOCKER_DIR
 
 cp $CORALWATCH_WEBAPP/Dockerfile $DOCKER_DIR
 cp $CORALWATCH_WEBAPP/target/coralwatch.war $DOCKER_DIR
